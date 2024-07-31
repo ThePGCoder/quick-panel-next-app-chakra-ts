46a130886fbd97987bce9f12b4654485e718a850
PGCwebdesigns <66938663+Philz1977@users.noreply.github.com>
2024-07-31 21:47:53 +1200

    first commit

---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "var(--font-poppins)",
+    body: "var(--font-poppins)",
+  },
+  config: {
+    initialColorMode: "light",
+  },
+});
