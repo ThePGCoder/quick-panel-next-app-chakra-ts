55fe4b7ace48e838745f4f9138e785adcfda35e4
PGCwebdesigns <66938663+Philz1977@users.noreply.github.com>
2024-08-04 20:34:26 +1200

    first commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,6 +13,7 @@ import {
   Text,
   useColorModeValue,
   Link,
+  Box,
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -31,70 +32,72 @@ const Login: React.FC<LoginProps> = () => {
   const router = useRouter();
   return (
     <>
-      <CustomCard>
-        <Stack spacing={4}>
-          <Center>
-            <Heading>LOGO</Heading>
-          </Center>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon
-                icon="mdi:email"
-                height={20}
-                color={useColorModeValue(primary, secondary)}
+      <Box width="90%">
+        <CustomCard>
+          <Stack spacing={4}>
+            <Center>
+              <Heading>LOGO</Heading>
+            </Center>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon
+                  icon="mdi:email"
+                  height={20}
+                  color={useColorModeValue(primary, secondary)}
+                />
+              </InputLeftElement>
+              <Input
+                type="email"
+                placeholder="Email"
+                borderColor={useColorModeValue(lightBorder, darkBorder)}
               />
-            </InputLeftElement>
-            <Input
-              type="email"
-              placeholder="Email"
-              borderColor={useColorModeValue(lightBorder, darkBorder)}
-            />
-          </InputGroup>
-
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon
-                icon="mdi:lock"
-                height={20}
-                color={useColorModeValue(primary, secondary)}
+            </InputGroup>
+  
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon
+                  icon="mdi:lock"
+                  height={20}
+                  color={useColorModeValue(primary, secondary)}
+                />
+              </InputLeftElement>
+              <Input
+                placeholder="Password"
+                type="password"
+                borderColor={useColorModeValue(lightBorder, darkBorder)}
               />
-            </InputLeftElement>
-            <Input
-              placeholder="Password"
-              type="password"
-              borderColor={useColorModeValue(lightBorder, darkBorder)}
-            />
-            <InputRightElement>
-              <Icon icon="mdi-eye" height={20} />
-            </InputRightElement>
-          </InputGroup>
-          <Center>
-            <Text fontSize="sm">
-              <Link color={useColorModeValue(primary, secondary)}>
-                Forgot Password?
-              </Link>
-            </Text>
-          </Center>
-          <Center>
-            <SocialProviders />
-          </Center>
-
-          <CustomButton width="100%" onClick={() => router.push("/home")}>
-            Login
-          </CustomButton>
-          <Center>
-            <Text fontSize="sm">
-              Don&apos;t have an account?{" "}
-              <Link
-                color={useColorModeValue(primary, secondary)}
-                href="/register"
-              >
-                Register
-              </Link>
-            </Text>
-          </Center>
-        </Stack>
-      </CustomCard>
+              <InputRightElement>
+                <Icon icon="mdi-eye" height={20} />
+              </InputRightElement>
+            </InputGroup>
+            <Center>
+              <Text fontSize="sm">
+                <Link color={useColorModeValue(primary, secondary)}>
+                  Forgot Password?
+                </Link>
+              </Text>
+            </Center>
+            <Center>
+              <SocialProviders />
+            </Center>
+  
+            <CustomButton width="100%" onClick={() => router.push("/home")}>
+              Login
+            </CustomButton>
+            <Center>
+              <Text fontSize="sm">
+                Don&apos;t have an account?{" "}
+                <Link
+                  color={useColorModeValue(primary, secondary)}
+                  href="/register"
+                >
+                  Register
+                </Link>
+              </Text>
+            </Center>
+          </Stack>
+        </CustomCard>
+      </Box>
     </>
   );
 };
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Text, Center, Heading, Input, InputGroup, InputLeftElement, InputRightElement, Link, Stack, useColorModeValue } from '@chakra-ui/react';
+import { Text, Center, Heading, Input, InputGroup, InputLeftElement, InputRightElement, Link, Stack, useColorModeValue, Box } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { Icon } from "@iconify/react";
@@ -16,74 +16,76 @@ const Register: React.FC<RegisterProps> = () => {
     const router = useRouter();
   return (
     <>
-      <CustomCard>
-        <Stack spacing={4}>
-          <Center>
-            <Heading>LOGO</Heading>
-          </Center>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon
-                icon="mdi:email"
-                height={20}
-                color={useColorModeValue(primary, secondary)}
+      <Box width="90%">
+        <CustomCard>
+          <Stack spacing={4}>
+            <Center>
+              <Heading>LOGO</Heading>
+            </Center>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon
+                  icon="mdi:email"
+                  height={20}
+                  color={useColorModeValue(primary, secondary)}
+                />
+              </InputLeftElement>
+              <Input
+                type="email"
+                placeholder="Email"
+                borderColor={useColorModeValue(lightBorder, darkBorder)}
               />
-            </InputLeftElement>
-            <Input
-              type="email"
-              placeholder="Email"
-              borderColor={useColorModeValue(lightBorder, darkBorder)}
-            />
-          </InputGroup>
-
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon
-                icon="mdi:lock"
-                height={20}
-                color={useColorModeValue(primary, secondary)}
+            </InputGroup>
+  
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon
+                  icon="mdi:lock"
+                  height={20}
+                  color={useColorModeValue(primary, secondary)}
+                />
+              </InputLeftElement>
+              <Input
+              type="password"
+                placeholder="Password"
+                borderColor={useColorModeValue(lightBorder, darkBorder)}
               />
-            </InputLeftElement>
-            <Input
-            type="password"
-              placeholder="Password"
-              borderColor={useColorModeValue(lightBorder, darkBorder)}
-            />
-            <InputRightElement>
-              <Icon icon="mdi-eye" height={20} />
-            </InputRightElement>
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon
-                icon="mdi:lock"
-                height={20}
-                color={useColorModeValue(primary, secondary)}
+              <InputRightElement>
+                <Icon icon="mdi-eye" height={20} />
+              </InputRightElement>
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon
+                  icon="mdi:lock"
+                  height={20}
+                  color={useColorModeValue(primary, secondary)}
+                />
+              </InputLeftElement>
+              <Input
+              type="password"
+                placeholder="Confirm Password"
+                borderColor={useColorModeValue(lightBorder, darkBorder)}
               />
-            </InputLeftElement>
-            <Input
-            type="password"
-              placeholder="Confirm Password"
-              borderColor={useColorModeValue(lightBorder, darkBorder)}
-            />
-            <InputRightElement>
-              <Icon icon="mdi-eye" height={20} />
-            </InputRightElement>
-          </InputGroup>
-          <Center>
-            <SocialProviders />
-          </Center>
-
-          <CustomButton width="100%" onClick={() => router.push("/home")}>
-            Register
-          </CustomButton>
-          <Center>
-            <Text fontSize="sm">
-              Already have an account? <Link color={useColorModeValue(primary, secondary)} href="/login">Login</Link>
-            </Text>
-          </Center>
-        </Stack>
-      </CustomCard>
+              <InputRightElement>
+                <Icon icon="mdi-eye" height={20} />
+              </InputRightElement>
+            </InputGroup>
+            <Center>
+              <SocialProviders />
+            </Center>
+  
+            <CustomButton width="100%" onClick={() => router.push("/home")}>
+              Register
+            </CustomButton>
+            <Center>
+              <Text fontSize="sm">
+                Already have an account? <Link color={useColorModeValue(primary, secondary)} href="/login">Login</Link>
+              </Text>
+            </Center>
+          </Stack>
+        </CustomCard>
+      </Box>
     </>
   );
 };
