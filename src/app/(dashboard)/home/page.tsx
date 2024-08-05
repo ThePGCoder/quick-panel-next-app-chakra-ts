0b2632c3ada35cb96208178efd5239b62e8657c9
PGCwebdesigns <66938663+Philz1977@users.noreply.github.com>
2024-08-05 19:02:39 +1200

    first commit

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+"use client"
+
+import { ActiveRouteContext } from '@/lib/hooks/activeRouteContext';
+import React, { useContext, useEffect } from 'react';
 
 interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = () => {
+    const { activeRoute, changeActiveRoute } = useContext(ActiveRouteContext);
+    useEffect(() => {
+        changeActiveRoute("Home");
+    },[])
     return (
         <>
             Home
